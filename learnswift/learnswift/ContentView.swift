6099533fc87043391a89e863c2532c6804ba4dde
wander <wander@ffactory.org>
2023-04-27 15:38:16 +0800

    Stack布局

---
+++ learnswift/learnswift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  learnswift
+//
+//  Created by lsmiao on 2023/4/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ learnswift/learnswift/learnswiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnswiftApp.swift
+//  learnswift
+//
+//  Created by lsmiao on 2023/4/27.
+//
+
+import SwiftUI
+
+@main
+struct learnswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
