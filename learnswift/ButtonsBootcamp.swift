8644e359cd6be5e2c259a2f7e10bd5eef88151c1
wander <wander@ffactory.org>
2023-04-28 10:29:34 +0800

    Update ButtonsBootcamp.swift

--- learnswift/ButtonsBootcamp.swift
+++ learnswift/ButtonsBootcamp.swift
@@ -21,26 +21,30 @@ struct ButtonsBootcamp: View {
                 self.title = "Button #2 was pressed"
             }, label: {
                 Text("Save".uppercased())
-                    .font(.headline)
-                    .fontWeight(.semibold)
-                    .foregroundColor(.white)
-                    .padding()
-                    .padding(.horizontal, 20)
+                    .font(.headline) // 字体
+                    .fontWeight(.semibold) // 字体粗细
+                    .foregroundColor(.white)// 前景颜色
+                    .padding() // 内边距
+                    .padding(.horizontal, 20) // 水平方向内边距
+                // 背景
                     .background(
                         Color.blue
-                            .cornerRadius(10)
-                            .shadow(radius: 10)
+                            .cornerRadius(10) // 圆角半径
+                            .shadow(radius: 10) // 阴影
+
                         
                     )
+
             })
             
             Button(action: {
                 self.title = "Button #3"
             }, label: {
                 Circle()
-                    .fill(Color.white)
-                    .frame(width: 75, height: 75 )
+                    .fill(Color.white) // 设置填充
+                    .frame(width: 75, height: 75 ) // 设置框架大小
                     .shadow(radius: 10)
+                // 覆盖图片等
                     .overlay(
                         Image(systemName: "heart.fill")
                             .font(.largeTitle)
@@ -58,8 +62,8 @@ struct ButtonsBootcamp: View {
                     .padding()
                     .padding(.horizontal,10)
                     .background(
-                   Capsule()
-                    .stroke(Color.gray, lineWidth: 2.0)
+                   Capsule()// 胶囊形状
+                    .stroke(Color.gray, lineWidth: 2.0) // 设置画笔的颜色和线条宽度
                         
                     )
             })
